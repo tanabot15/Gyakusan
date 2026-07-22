dff6348e387790e5799b7f06dabd0e295a8c8923
tanabot15 <kenichirosuzuki1@gmail.com>
2026-07-22 16:14:39 +0900

    Initial Commit

---
+++ Gyakusan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gyakusan/GyakusanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyakusanApp.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/07/22.
+//
+
+import SwiftUI
+
+@main
+struct GyakusanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
