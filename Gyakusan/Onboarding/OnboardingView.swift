8e67316b32363829832bad1891d07f52f7c49785
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-12 08:12:33 +0900

    Add OnboardingView

---
+++ Gyakusan/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/08/12.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
