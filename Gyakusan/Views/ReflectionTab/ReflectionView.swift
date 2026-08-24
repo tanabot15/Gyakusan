d0eca01c9eb18a903d983e3e33255f432d88d921
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-24 13:42:41 +0900

    Add ReflectionView

---
+++ Gyakusan/Views/ReflectionTab/ReflectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ReflectionView.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/08/24.
+//
+
+import SwiftUI
+
+struct ReflectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReflectionView()
+}
