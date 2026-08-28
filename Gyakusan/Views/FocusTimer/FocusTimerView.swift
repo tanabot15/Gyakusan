e87caf8361bab0be0d71cbf4f7d19822207ef990
tanabot15 <kenichirosuzuki1@gmail.com>
2026-08-28 21:29:54 +0900

    Simplify TodoListView

---
+++ Gyakusan/Views/FocusTimer/FocusTimerView.swift
@@ -0,0 +1,18 @@
+//
+//  FocusTimerView.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/08/27.
+//
+
+import SwiftUI
+
+struct FocusTimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusTimerView()
+}
