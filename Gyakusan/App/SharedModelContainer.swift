2baf8460d776f026138375ad5f308afb41a07ac9
tanabot15 <kenichirosuzuki1@gmail.com>
2026-09-07 21:14:32 +0900

    Add Widget

---
+++ Gyakusan/App/SharedModelContainer.swift
@@ -0,0 +1,8 @@
+//
+//  SharedModelContainer.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/09/07.
+//
+
+import Foundation
---
+++ GyakusanWidget/GyakusanWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  GyakusanWidgetBundle.swift
+//  GyakusanWidget
+//
+//  Created by Kenichiro Suzuki on 2026/09/07.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct GyakusanWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        GyakusanWidget()
+    }
+}
