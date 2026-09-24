4ed20b2504d0fd7a0190fefef58545ff0d760f0f
tanabot15 <kenichirosuzuki1@gmail.com>
2026-09-24 12:59:22 +0900

    Change color of Widget grid

---
+++ Gyakusan/Views/Utilities/FocusTimerAttributes.swift
@@ -0,0 +1,8 @@
+//
+//  FocusTimerAttributes.swift
+//  Gyakusan
+//
+//  Created by Kenichiro Suzuki on 2026/09/14.
+//
+
+import Foundation
---
+++ GyakusanWidget/FocusTimerLiveActivity.swift
@@ -0,0 +1,8 @@
+//
+//  FocusTimerLiveActivity.swift
+//  GyakusanWidgetExtension
+//
+//  Created by Kenichiro Suzuki on 2026/09/14.
+//
+
+import Foundation
